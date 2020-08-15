b8eec9689ba026a936395b98bee79f30dc18088b
ThibaudLk <thibauuud@gmail.com>
2020-08-15 18:54:04 +0200

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteMetadata: {
-		title: 'Gatsby - Bulma CSS Starter',
-		author: 'Aman Mittal',
+		title: 'Espace KGnaar',
+		author: 'Thibaud Larrière',
 		imageUrl: 'https://i.imgur.com/Vz81GEl.png',
 		description: 'A Project to bootstrap your next Gatsby + Bulma site.',
 		keywords: `Web developer, Web, Developer, CSS, HTML, JS, Javascript, Gatsby, Bulma Developer, CSS3, HTML5, Seo, Starter`,
